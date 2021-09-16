0ceb45d06650131b7ffc7be98d78f77ea754846f
Demian <dpalumbosctyp@gmail.com>
2021-09-16 20:13:26 -0300

    comienzo del proyecto

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+#Declaro variables
+#Ingreso número
+num1 = float(input('ingresar un número:'))
+num2 = float(input('ingresar otro numero: '))
+#realizamos suma y guardamos resultado
+resultado = num1 + num2
+#imprimimos resultado en pantalla
+print (f'el resultado es: {resultado}')
